304fb24536fbcb852d3a244874436174b8307dba
Martin <darkmtbg2@gmail.com>
2022-10-03 10:25:18 +0200

    Starting setup for login

--- app/src/main/java/com/example/quarantaine/MainActivity.java
+++ app/src/main/java/com/example/quarantaine/MainActivity.java
@@ -4,16 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.quarantaine.DTO.APIResponse;
+import com.example.quarantaine.service.JSONApiCaller;
+import com.example.quarantaine.service.StringAPICaller;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        try {
+
+            FutureTask<APIResponse> task = new FutureTask<APIResponse>(new StringAPICaller("/api/token", "username", "Passw0rd!"));
+
+            APIResponse result = null;
+
+            new Thread(task).start();
+            Log.i("app", "Callng api Response");
+            result = task.get(5, TimeUnit.SECONDS);
+
+            Log.i("app", "Got Response");
+        }
+        catch (Exception e){
+            Log.i("app", "Some exception");
+        }
     }
 
 
---
+++ app/src/main/java/com/example/quarantaine/service/BaseApiCaller.java
@@ -0,0 +1,99 @@
+package com.example.quarantaine.service;
+
+import android.util.Log;
+
+import com.example.quarantaine.DTO.APIResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class BaseApiCaller {
+
+
+    public APIResponse getAPIData(String baseURL,String methodType ,String apiEndpoint, Map<String,String> header, String body){
+        APIResponse response = null;
+        HttpsURLConnection connection = null;
+        try{
+            URL urlobj = new URL(baseURL + apiEndpoint);
+            connection = (HttpsURLConnection) urlobj.openConnection();
+
+            if(connection == null){
+                Log.i("app", "WHy is connection null");
+            }
+
+            connection.setRequestMethod(methodType);
+            for (Map.Entry<String,String> entry: header.entrySet()) {
+                connection.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+            //connection.setRequestProperty("content-type", "application/json");
+            //extra data
+            //connection.setRequestProperty(map.key, map.data);
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            connection.setDoOutput(true);
+            BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            Log.i("JSON", "Converted JSON OBJECT");
+
+            wr.write(body);
+            wr.flush();
+            wr.close();
+
+            Log.i("app", "wrote data to stream");
+            connection.connect();
+            Log.i("app", "connected");
+
+            int responsecode = connection.getResponseCode();
+            StringBuffer responseString = new StringBuffer();
+
+            try{
+                BufferedReader in;
+                if(responsecode >= 200 && responsecode < 300){
+                    in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                }
+                else{
+                    in = new BufferedReader(new InputStreamReader(connection.getErrorStream(),StandardCharsets.UTF_8));
+                }
+
+                String input;
+
+                while ((input = in.readLine()) != null){
+                    responseString.append(input);
+                }
+
+                in.close();
+                Log.i("app", "Read data from stream");
+            }
+            catch (Exception e){
+                Log.i("app", "tried to read data from stream failed");
+            }
+
+            Log.i("app", "full response :" + (responseString == null ? "null": responseString));
+            response = new APIResponse(responseString.toString(),responsecode);
+        }
+        catch(Exception e)
+        {
+            Log.i("app", e.getLocalizedMessage());
+        }
+        finally {
+            if(connection != null){
+                connection.disconnect();
+                Log.i("app","Closed connection");
+            }
+        }
+        Log.i("app",response.getResponseBody());
+        Log.i("app",new Integer(response.getResponseCode()).toString());
+
+        return response;
+
+    }
+
+}
